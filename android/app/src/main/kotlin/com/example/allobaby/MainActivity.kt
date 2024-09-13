17ceb098e59c2576691d47ba25818d6e3644fbb8
Vijay Sundar <112871288+sucvijay@users.noreply.github.com>
2024-09-13 11:26:22 +0530

    Revert "latest"
    This reverts commit 76ca7fa75937eb4899291c2fa8ef1ef607c8c53f.

--- android/app/src/main/kotlin/com/example/allobaby/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/allobaby/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.allobaby
 
-
+import com.otpless.otplessflutter.OtplessFlutterPlugin
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
-
-import com.otpless.otplessflutter.OtplessFlutterPlugin;
-import android.content.Intent;
 
 class MainActivity : FlutterActivity() {
 
-override fun onNewIntent(intent: Intent) {
-    super.onNewIntent(intent)
-    val plugin = flutterEngine?.plugins?.get(OtplessFlutterPlugin::class.java)
-    if (plugin is OtplessFlutterPlugin) {
-        plugin.onNewIntent(intent)
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val plugin = flutterEngine?.plugins?.get(OtplessFlutterPlugin::class.java)
+        if (plugin is OtplessFlutterPlugin) {
+            plugin.onNewIntent(intent)
+        }
     }
-}
 
     override fun onBackPressed() {
         val plugin = flutterEngine?.plugins?.get(OtplessFlutterPlugin::class.java)
--- lib/Controller/AuthController.dart
+++ lib/Controller/AuthController.dart
@@ -2,12 +2,9 @@ import 'package:allobaby/API/apiroutes.dart';
 import 'package:allobaby/Screens/mobileverification/otpverification.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:otpless_flutter/otpless_flutter.dart';
 
 class Authcontroller extends GetxController {
 TextEditingController phone = TextEditingController();
-
-Otpless otpLess = Otpless();
 
   onSuccessLogin() {
     print(Apiroutes().getUrl("/login"));
